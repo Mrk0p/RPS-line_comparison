7b69a577c57ce035e50623bc9bf781f6c7e7f227
Mrk0p <sachinwele23@gamil.com>
2023-06-17 20:41:23 +0530

    Added: line comparison program

---
+++ src/com/bridgelabz/line_comparison.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class line_comparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to line comparison computation program .");
+
+    }
+}
