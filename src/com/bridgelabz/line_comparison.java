13f6d228c16001aee85e0aa42b7ae633ec496378
Mrk0p <sachinwele23@gamil.com>
2023-06-17 20:44:01 +0530

    Added: calculate line length

--- src/com/bridgelabz/line_comparison.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz;
-
-public class line_comparison {
-    public static void main(String[] args) {
-        System.out.println("Welcome to line comparison computation program .");
-
-    }
-}
